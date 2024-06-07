af58ac5aaef0af748ec3949580a85a1ec70edc69
novary <ryannovarypradana@gmail.com>
2024-06-07 09:29:28 +0700

    add css to components

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,21 @@ function App() {
   console.log(todos)
 
   return (
-    <div>
-    <h1>My Todo List</h1>
-    {/* Berikan data-nya ke component Todos */}
-    <Todos todos={todos} />
-  </div>
+    <div style={styles.container}>
+      <h1 style={styles.title}>My Todo List</h1>
+      <Todos todos={todos} />
+    </div>
   )
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    padding: '12px',
+  },
+  title: {
+    fontSize: '36px',
+  },
 }
 
 export default App
